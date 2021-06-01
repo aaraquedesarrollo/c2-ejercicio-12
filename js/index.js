d482d60dc2d27542618130ef5c7e24b15e5fcbae
Albert Araque <aaraquedesarrollo@gmail.com>
2021-06-01 11:48:57 +0200

    funcion comprobarNumero completada

--- js/index.js
+++ js/index.js
@@ -3,16 +3,26 @@ const numeroGenerado = generarNumero();
 
 const botonAdivinar = document.querySelector(".adivinar");
 const numeroElemento = document.querySelector(".numero");
-botonAdivinar.addEventListener("click", (e) =>
-  comprobarNumero(numeroElemento.textContent)
-);
+botonAdivinar.addEventListener("click", (e) => {
+  if (botonAdivinar.valueAsNumber !== undefined) {
+    comprobarNumero(numeroElemento.valueAsNumber);
+  }
+});
 
 const comprobarNumero = (numeroInput) => {
+  const mensajeElemento = document.querySelector(".mensaje");
   if (numeroInput > numeroGenerado) {
-    //
+    mensajeElemento.textContent = "El numero que has introducido es mas grande";
+    mensajeElemento.classList.add("error");
   } else if (numeroInput < numeroGenerado) {
-    //
+    mensajeElemento.textContent =
+      "El numero que has introducido es mas pequeño";
+    mensajeElemento.classList.add("error");
   } else {
-    //
+    mensajeElemento.textContent = "Has acertado!";
+    mensajeElemento.classList.remove("error");
+    mensajeElemento.classList.add("acierto");
+    botonAdivinar.disabled = true;
+    numeroElemento.disabled = true;
   }
 };
