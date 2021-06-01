ab0129f1de981665710d82f669fed222d6c145e5
Albert Araque <aaraquedesarrollo@gmail.com>
2021-06-01 10:53:51 +0200

    añadido eventListener al boton

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
 const generarNumero = () => Math.floor(Math.random() * (10 - 1) + 1);
 const numeroGenerado = generarNumero();
+
+const botonAdivinar = document.querySelector(".adivinar");
+botonAdivinar.addEventListener("click", (e) => comprobarNumero());
